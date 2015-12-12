c637e5ebd2831ecad02c8da7c0f1936a4a446720
SiloSas <loann@debian>
2015-12-12 20:29:25 +0100

    first

---
+++ client/src/main/scala/App.scala
@@ -0,0 +1,20 @@
+import Room.{RoomController, RoomMinDirective}
+import com.greencatsoft.angularjs._
+import example.RoomServiceFactory
+
+import scala.scalajs.js.JSApp
+import scala.scalajs.js.annotation.JSExport
+
+@JSExport
+object App extends JSApp {
+
+  override def main() {
+    val module = Angular.module("app", Seq("ngAnimate", "ngAria", "ngMaterial", "mm.foundation", "ngRoute", "ngMap"))
+
+    module
+    .factory[RoomServiceFactory]
+    .controller[RoomController]
+    .directive[RoomMinDirective]
+    .config(RoutingConfig)
+  }
+}
